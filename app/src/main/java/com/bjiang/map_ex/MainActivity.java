ca781b61f1c8c49541ef88c0d329cab3aec441e9
Bojie Jiang <jiangbojie@gmail.com>
2015-01-26 21:56:24 -0800

    add start and end address and have getDirections

--- app/src/main/java/com/bjiang/map_ex/MainActivity.java
+++ app/src/main/java/com/bjiang/map_ex/MainActivity.java
@@ -1,18 +1,33 @@
 package com.bjiang.map_ex;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends Activity {
@@ -21,7 +36,7 @@ public class MainActivity extends Activity {
     private GoogleMap googleMap;
     private EditText et_address, et_finalAddress;
     LatLng addressPos, finalAddressPos;
-    Marker addressMarkerp;
+    Marker addressMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +50,7 @@ public class MainActivity extends Activity {
         // Initial Map
         try {
 
-            if(googleMap == null) {
+            if (googleMap == null) {
                 googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
             }
         } catch (Exception e) {
@@ -84,20 +99,117 @@ public class MainActivity extends Activity {
 
         String newAddress = et_address.getText().toString();
 
-        if(newAddress != null) {
-            new PlaceAMarker().exeute();
+        if (newAddress != null) {
+            new PlaceAMarker().execute(newAddress);
         }
     }
 
     public void getDirections(View view) {
+
+        String startingAddress = et_address.getText().toString();
+        String finalAddress = et_finalAddress.getText().toString();
+
+        if ((startingAddress.equals("")) || finalAddress.equals("")) {
+            Toast.makeText(this, "Enter a starting and Ending address", Toast.LENGTH_SHORT).show();
+        } else {
+            new GetDirections().execute(startingAddress, finalAddress);
+        }
     }
 
-    class PlaceAMarker extends AsyncTask<String, String, String>{
+    class PlaceAMarker extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... params) {
             String startAddress = params[0];
+            startAddress = startAddress.replaceAll(" ", "%20");
+
+            getLatLng(startAddress, false);
+
             return null;
         }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            addressMarker = googleMap.addMarker(new MarkerOptions()
+            .position(addressPos).title("Address"));
+        }
+    }
+
+    class GetDirections extends AsyncTask<String, String, String>{
+        @Override
+        protected String doInBackground(String... params) {
+            String startAddress =  params[0];
+            startAddress = startAddress.replaceAll(" ", "%20");
+            getLatLng(startAddress, false);
+
+            String endingAddress = params[1];
+            endingAddress = endingAddress.replaceAll(" ", "%20");
+            getLatLng(endingAddress, true);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            String geoUriString = "http://maps.google.com/maps?addr=" +
+                    addressPos.latitude + "," +
+                    addressPos.longitude + "&daddr=" +
+                    finalAddressPos.latitude + "," +
+                    finalAddressPos.longitude;
+
+            Intent mapCall = new Intent(Intent.ACTION_VIEW, Uri.parse(geoUriString));
+            startActivity(mapCall);
+        }
+    }
+
+    protected void getLatLng(String address, boolean setDestination) {
+        String uri = "http://maps.google.com/maps/api/geocode/json?address="
+                + address + "&sensor=false";
+
+        HttpGet httpGet = new HttpGet(uri);
+
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            response = client.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+
+            InputStream stream = entity.getContent();
+
+            int byteData;
+            while ((byteData = stream.read()) != -1) {
+                stringBuilder.append((char) byteData);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        double lat = 0.0, lng = 0.0;
+
+        JSONObject jsonObject;
+        try {
+            jsonObject = new JSONObject(stringBuilder.toString());
+            lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lng");
+            lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
+                    .getJSONObject("geometry").getJSONObject("location")
+                    .getDouble("lat");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (setDestination) {
+            finalAddressPos = new LatLng(lat, lng);
+        } else {
+            addressPos = new LatLng(lat, lng);
+        }
+
     }
 }
